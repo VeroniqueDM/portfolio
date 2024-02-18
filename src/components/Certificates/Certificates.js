a8e25227d6c538a90366a78330b620fc1abcf267
VeroniqueDM <veronika.dmiteva@gmail.com>
2024-02-18 21:35:43 +0100

    updated certificates links

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -28,85 +28,106 @@ function Certificates() {
                             <a
                                 target="_blank"
                                 rel="nooper noreferrer"
-                                href="https://softuni.bg/certificates/details/119418/c0965617"
+                                // href="https://softuni.bg/certificates/details/119418/c0965617"
+                                href={process.env.PUBLIC_URL + "/certificates/Programming Fundamentals with Python - September 2021 - Certificate.pdf"}
+
                             >
                                 Programming Fundamentals with Python
                             </a>
                             <a
-                                href="https://softuni.bg/certificates/details/131172/290e1593"
+                                // href="https://softuni.bg/certificates/details/131172/290e1593"
                                 target="_blank"
                                 rel="nooper noreferrer"
+                                href={process.env.PUBLIC_URL + "/certificates/Python OOP - February 2022 - Certificate.pdf"}
+
                             >
                                 Python OOP
                             </a>
                             <a
-                                href="https://softuni.bg/certificates/details/126254/96241312"
+                                // href="https://softuni.bg/certificates/details/126254/96241312"
                                 target="_blank"
                                 rel="nooper noreferrer"
+                                href={process.env.PUBLIC_URL + "/certificates/Python Advanced - January 2022 - Certificate.pdf"}
+
                             >
                                 Python Advanced
                             </a>
 
                             <a
-                                href="https://softuni.bg/certificates/details/133036/872a2c5c"
+                                // href="https://softuni.bg/certificates/details/133036/872a2c5c"
                                 target="_blank"
                                 rel="nooper noreferrer"
+                                href={process.env.PUBLIC_URL + "/certificates/Python Web Basics - January 2022 - Certificate.pdf"}
+
                             >
                                 Python Web Basics
                             </a>
                             <a
-                                href="https://softuni.bg/certificates/details/133145/c3077154"
+                                // href="https://softuni.bg/certificates/details/133145/c3077154"
                                 target="_blank"
                                 rel="nooper noreferrer"
+                                href={process.env.PUBLIC_URL + "/certificates/Python Web Framework - March 2022 - Certificate.pdf"}
+
                             >
                                 Python Web Framework
                             </a>
 
                             <a
-                                href="https://softuni.bg/certificates/details/139920/9d461a9c"
+                                // href="https://softuni.bg/certificates/details/139920/9d461a9c"
                                 target="_blank"
                                 rel="nooper noreferrer"
+                                href={process.env.PUBLIC_URL + "/certificates/Algorithms with Python - July 2022 - Certificate.pdf"}
+
                             >
                                 Algorithms with Python
                             </a>
                             <a
-                                href="https://softuni.bg/certificates/details/136649/74f0e4d9"
+                                // href="https://softuni.bg/certificates/details/136649/74f0e4d9"
                                 target="_blank"
                                 rel="nooper noreferrer"
+                                href={process.env.PUBLIC_URL + "/certificates/JS Advanced - May 2022 - Certificate.pdf"}
+
                             >
                                 JS Advanced
                             </a>
                             <a
-                                href="https://softuni.bg/certificates/details/139747/287a77a3"
+                                // href="https://softuni.bg/certificates/details/139747/287a77a3"
                                 target="_blank"
                                 rel="nooper noreferrer"
+                                href={process.env.PUBLIC_URL + "/certificates/JS Applications - June 2022 - Certificate.pdf"}
+
                             >
                                 JS Applications
                             </a>
 
                             <a
-                                href="https://softuni.bg/certificates/details/174680/fbe6c62f"
+                                // href="https://softuni.bg/certificates/details/174680/fbe6c62f"
                                 target="_blank"
                                 rel="nooper noreferrer"
+                                href={process.env.PUBLIC_URL + "/certificates/Java Advanced - May 2023 - Certificate.pdf"}
+
                             >
                                 Java Advanced
                             </a>
                             <a
-                                href="https://softuni.bg/certificates/details/181540/2a92d1a1"
+                                // href="https://softuni.bg/certificates/details/181540/2a92d1a1"
                                 target="_blank"
                                 rel="nooper noreferrer"
+                                href={process.env.PUBLIC_URL + "/certificates/Java OOP - June 2023 - Certificate.pdf"}
+
                             >
                                 Java OOP
                             </a>
                             <a
-                                href="https://creative.softuni.bg/certificates/details/21546/7d55ab0c"
+                                // href="https://creative.softuni.bg/certificates/details/21546/7d55ab0c"
                                 target="_blank"
                                 rel="nooper noreferrer"
+                                
                             >
                                 Design Basics (Honorable Mention)
                             </a>
                             <a
-                                href="https://digital.softuni.bg/certificates/details/32731/fa763065"
+                                // href="https://digital.softuni.bg/certificates/details/32731/fa763065"
                                 target="_blank"
                                 rel="nooper noreferrer"
                             >
